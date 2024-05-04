3278984e4bfca1da61d07ebe5cc4ab1387c8db5b
beluguilover <almabrapal@gmail.com>
2024-05-04 02:21:28 +0200

    Intento

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.wallitapp"
-    compileSdk = 34
+    compileSdk = 33
 
     defaultConfig {
         applicationId = "com.example.wallitapp"
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,4 +21,5 @@ dependencyResolutionManagement {
 
 rootProject.name = "WallItApp"
 include(":app")
+include(":app")
  
